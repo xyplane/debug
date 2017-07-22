6068a5b39889525e0e5c16e2ac935e8df2cd42b8
xyplane <planar.geometry@gmail.com>
2017-07-22 07:42:12 -0400

    Refine original API and add initial tests

--- debug.go
+++ debug.go
@@ -1,167 +1,203 @@
-package debug
+package debugger
 
 import (
-    "os"
-    "fmt"
-    "time"
-    "sync"
-    "regexp"
-    "strings"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strings"
+	"sync"
+	"time"
 )
 
-var spec = struct {
-   sync.Mutex
-   initialized bool
-   includes []*regexp.Regexp
-   excludes []*regexp.Regexp
-}{}
+var mtx sync.Mutex
+var includes []*regexp.Regexp
+var excludes []*regexp.Regexp
+var output io.Writer = os.Stderr
+var last = time.Now()
 
-var last = struct {
-    sync.Mutex
-    now time.Time
-}{
-    now: time.Now(),
-}
-
-var output = os.Stderr
-
-type loggerType int
+type outputType int
 
 const (
-    debugType   loggerType = iota
-    debugfType
-    debuglnType
+	printType outputType = iota
+	printfType
+	printlnType
+	enabledType
+	nameType
 )
 
-var placeholder_regexp = regexp.MustCompile("%[0-9.+\\-\\[\\]#]*[vTt%bcdoqUespEfFgGxX]")
-
-
-type DebugLogger func(a ...interface{})
-
-func (logger DebugLogger) Debug(args ...interface{}) {
-    logger(append([]interface{}{ debugType }, args...)...)
+func init() {
+	parse(os.Getenv("DEBUG"))
 }
 
-func (logger DebugLogger) F(format string, args ...interface{}) {
-    logger(append([]interface{}{ debugfType, format }, args...)...)
+var newlineSuffix = []byte("\n")
+
+var formatRegExp = regexp.MustCompile("%[0-9.+\\-\\[\\]#]*[vTt%bcdoqUespEfFgGxX]")
+
+type Logger func(a ...interface{})
+
+func (log Logger) Ln(a ...interface{}) {
+	log.Println(a...)
 }
 
-func (logger DebugLogger) Debugf(format string, args ...interface{}) {
-    logger(append([]interface{}{ debugfType, format }, args...)...)
+func (log Logger) F(format string, a ...interface{}) {
+	log.Printf(format, a...)
 }
 
-func (logger DebugLogger) Ln(args ...interface{}) {
-    logger(append([]interface{} { debuglnType }, args...)...)
+func (log Logger) Print(a ...interface{}) {
+	args := [10]interface{}{printType}
+	log(append(args[1:], a...)...)
 }
 
-func (logger DebugLogger) Debugln(args ...interface{}) {
-    logger(append([]interface{} { debuglnType }, args...)...)
+func (log Logger) Println(a ...interface{}) {
+	args := [10]interface{}{printlnType}
+	log(append(args[1:], a...)...)
 }
 
-
-func log(name string, args ...interface{}) {
-    last.Lock()
-    defer last.Unlock()
-
-    now := time.Now()
-    delta := int64(now.Sub(last.now)/time.Millisecond)
-    last.now = now
-
-    prefix := fmt.Sprintf("  %s +%dms: ", name, delta)
-
-    if len(args) == 0 {
-        fmt.Fprintln(output, prefix)
-        return
-    }
-
-    fmt.Fprint(output, prefix)
-
-    switch arg := args[0].(type) {
-    case loggerType:
-        switch arg {
-        case debugType:
-            fmt.Fprint(output, args[1:]...)
-        case debugfType:
-            fmt.Fprintf(output, fmt.Sprint(args[1]), args[2:]...)
-        case debuglnType:
-            fmt.Fprintln(output, args[1:]...)
-        }
-    case string:
-        if placeholder_regexp.MatchString(arg) {
-            if strings.HasPrefix(arg, "\n") {
-                fmt.Fprintf(output, arg, args[1:]...)
-            } else {
-                fmt.Fprintf(output, arg+"\n", args[1:]...)
-            }
-        } else {
-            fmt.Fprintln(output, args...)
-        }
-    default:
-        fmt.Fprintln(output, args...)
-    } 
+func (log Logger) Printf(format string, a ...interface{}) {
+	args := [10]interface{}{printfType}
+	log(append(args[1:], a...)...)
 }
 
-
-func initialize() {
-    if spec.initialized {
-        return
-    }
-
-    d := os.Getenv("DEBUG")
-
-    for _, pat := range(strings.Split(d, ",")) {
-        
-        exclude := false;
-        if strings.HasPrefix(pat, "-") {
-            pat = strings.TrimPrefix(pat, "-")
-            exclude = true
-        }
-
-        pat = regexp.QuoteMeta(pat)
-        pat = strings.Replace(pat, "\\*", ".*", -1)
-        re := regexp.MustCompile("^" + pat + "$")
-
-        if exclude {
-            spec.excludes = append(spec.excludes, re)
-        } else {
-            spec.includes = append(spec.includes, re)
-        }
-    }
-
-    spec.initialized = true
+func (log Logger) Enabled() bool {
+	var enabled bool
+	log(enabledType, &enabled)
+	return enabled
 }
 
-
-func Debug(name string) DebugLogger {
-    spec.Lock()
-    defer spec.Unlock()
-    initialize()
-
-    noopLogger := DebugLogger(func(a ...interface{}) {})
-
-    match := false
-    for _, re := range(spec.includes) {
-        if re.MatchString(name) {
-            match = true
-            break
-        }
-    }
-
-    if !match {
-        return noopLogger
-    }
-
-    for _, re := range(spec.excludes) {
-        if re.MatchString(name) {
-            match = false
-            break
-        }
-    }
-
-    if !match {
-        return noopLogger
-    }
-
-    return DebugLogger(func(a ...interface{}) { log(name, a...) })
+func (log Logger) Name() string {
+	var name string
+	log(nameType, &name)
+	return name
 }
 
+func (log Logger) Child(name string) Logger {
+	return Debug(log.Name() + ":" + name)
+}
+
+func handle(name string, enabled bool, a ...interface{}) {
+	if len(a) == 0 {
+		writeln(name, enabled)
+		return
+	}
+
+	switch cmd := a[0].(type) {
+	case outputType:
+		switch cmd {
+		case printType:
+			fallthrough
+		case printlnType:
+			writeln(name, enabled, a[1:]...)
+		case printfType:
+			if format, ok := a[1].(string); ok {
+				writef(name, enabled, format, a[2:]...)
+			} else {
+				panic("")
+			}
+		case enabledType:
+			if result, ok := a[1].(*bool); ok {
+				*result = enabled
+			} else {
+				panic("")
+			}
+		case nameType:
+			if result, ok := a[1].(*string); ok {
+				*result = name
+			} else {
+				panic("")
+			}
+		}
+	case string:
+		if formatRegExp.MatchString(cmd) {
+			writef(name, enabled, cmd, a[1:]...)
+		} else {
+			writeln(name, enabled, a...)
+		}
+	default:
+		writeln(name, enabled, a...)
+	}
+}
+
+func writeln(name string, enabled bool, a ...interface{}) {
+	if !enabled {
+		return
+	}
+	mtx.Lock()
+	defer mtx.Unlock()
+	buf := &bytes.Buffer{}
+	prefix(buf, name)
+	fmt.Fprintln(buf, a...)
+	output.Write(buf.Bytes())
+}
+
+func writef(name string, enabled bool, format string, a ...interface{}) {
+	if !enabled {
+		return
+	}
+	mtx.Lock()
+	defer mtx.Unlock()
+	buf := &bytes.Buffer{}
+	prefix(buf, name)
+	fmt.Fprintf(buf, format, a...)
+	if !bytes.HasSuffix(buf.Bytes(), newlineSuffix) {
+		buf.Write(newlineSuffix)
+	}
+	output.Write(buf.Bytes())
+}
+
+func prefix(w io.Writer, name string) {
+	now := time.Now()
+	delta, unit := int64(now.Sub(last)/time.Millisecond), "ms"
+	if delta > 1000 {
+		delta, unit = delta/1000, "s"
+	}
+	last = now
+	fmt.Fprintf(w, "  %s +%d%s: ", name, delta, unit)
+}
+
+func parse(spec string) {
+	mtx.Lock()
+	defer mtx.Unlock()
+
+	for _, pat := range strings.Split(spec, ",") {
+		exclude := false
+		if strings.HasPrefix(pat, "-") {
+			pat = strings.TrimPrefix(pat, "-")
+			exclude = true
+		}
+
+		pat = regexp.QuoteMeta(pat)
+		pat = strings.Replace(pat, "\\*", ".*", -1)
+		re := regexp.MustCompile("^" + pat + "$")
+
+		if exclude {
+			excludes = append(excludes, re)
+		} else {
+			includes = append(includes, re)
+		}
+	}
+}
+
+func Debug(name string) Logger {
+	match := false
+	for _, re := range includes {
+		if re.MatchString(name) {
+			match = true
+			break
+		}
+	}
+
+	if match {
+		for _, re := range excludes {
+			if re.MatchString(name) {
+				match = false
+				break
+			}
+		}
+	}
+
+	return Logger(func(a ...interface{}) {
+		handle(name, match, a...)
+	})
+}
---
+++ debug_test.go
@@ -0,0 +1,67 @@
+package debugger
+
+import (
+	"bytes"
+	"regexp"
+	"testing"
+	"time"
+)
+
+func TestDebugger(t *testing.T) {
+	pattern := "test:other,test:child*,-test:child2"
+	buffer := &bytes.Buffer{}
+	output = buffer
+
+	parse(pattern)
+
+	debug := Debug("test")
+	if debug.Name() != "test" {
+		t.Fatalf("Logger name expecting 'test' found '%s'", debug.Name())
+	}
+	if debug.Enabled() {
+		t.Fatalf("Logger '%s' is enabled for spec: '%s'", debug.Name(), pattern)
+	}
+
+	debug("Message for disabed logger")
+	if len(buffer.Bytes()) > 0 {
+		t.Fatalf("Logger '%s' has output when disabled: %s", debug.Name(), string(buffer.Bytes()))
+	}
+
+	other := debug.Child("other")
+	if other.Name() != "test:other" {
+		t.Fatalf("Logger name expecting 'test:other' found '%s'", other.Name())
+	}
+	if !other.Enabled() {
+		t.Fatalf("Logger '%s' is disabled for spec: '%s'", other.Name(), pattern)
+	}
+
+	other("Message for enabled logger")
+	if ok, _ := regexp.Match("  test:other \\+0ms: Message for enabled logger\n", buffer.Bytes()); !ok {
+		t.Fatalf("Logger '%s' unexpected output: '%s'", other.Name(), string(buffer.Bytes()))
+	}
+	buffer.Reset()
+
+	child1 := debug.Child("child1")
+	if child1.Name() != "test:child1" {
+		t.Fatalf("Logger name expecting 'test:child1' found '%s'", child1.Name())
+	}
+	if !child1.Enabled() {
+		t.Fatalf("Logger '%s' is disabled for spec: '%s'", child1.Name(), pattern)
+	}
+
+	time.Sleep(15 * time.Millisecond)
+
+	child1("Message for %s logger", child1.Name())
+	if ok, _ := regexp.Match("  test:child1 \\+1\\dms: Message for test:child1 logger\n", buffer.Bytes()); !ok {
+		t.Fatalf("Logger '%s' unexpected output: '%s'", child1.Name(), string(buffer.Bytes()))
+	}
+	buffer.Reset()
+
+	child2 := debug.Child("child2")
+	if child2.Name() != "test:child2" {
+		t.Fatalf("Logger name expecting 'test:child2' found '%s'", child2.Name())
+	}
+	if child2.Enabled() {
+		t.Fatalf("Logger '%s' is enabled for spec: '%s'", child2.Name(), pattern)
+	}
+}
